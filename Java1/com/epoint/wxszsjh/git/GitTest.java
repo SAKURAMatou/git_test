5c278759e21a921ae458b23f3eb3fecf42864e3f
“DML” <dailin1357@gmail.com>
2021-05-23 18:22:08 +0800

    dev分支提交

--- Java1/com/epoint/wxszsjh/git/GitTest.java
+++ Java1/com/epoint/wxszsjh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.epoint.wxszsjh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git测试");
+        System.out.println("分支dev");
     }
 }
